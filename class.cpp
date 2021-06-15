0abcdc4db0a1e0101f36cb466a8dd65768cea8d3
div-57 <dawid050702000@gmail.com>
2021-06-15 08:15:06 +0200

    Update class.cpp

--- class.cpp
+++ class.cpp
@@ -8,6 +8,23 @@ void Game::setWindowBounds(int left, int right, int top, int bottom)
     window_bound_bottom = bottom;
 }
 
+Player::Player(sf::Texture &texture, const int &resolution_x, const int &resolution_y, int HP, int speed)
+{
+    setTexture(texture);
+    setPosition((resolution_x/2) - getGlobalBounds().width/2, resolution_y - 178);
+    setWindowBounds(0, resolution_x, 0, resolution_y);
+    HP_ = HP;
+    speed_ = speed;
+}
+
+Enemy_red::Enemy_red(sf::Texture &texture, const int &resolution_x, int HP, int speed)
+{
+    setTexture(texture);
+    setPosition(rand() % (resolution_x - 121) + 1, -getGlobalBounds().height);
+    HP_ = HP;
+    speed_ = speed;
+}
+
 void Player::animate(const sf::Time &elapsed)
 {
     sf::FloatRect player_bounds = getGlobalBounds();
@@ -16,7 +33,7 @@ void Player::animate(const sf::Time &elapsed)
     {
         if(player_bounds.left >= window_bound_left)
         {
-            move(-speed * elapsed.asSeconds(), 0);
+            move(-speed_ * elapsed.asSeconds(), 0);
         }
     }
 
@@ -24,7 +41,7 @@ void Player::animate(const sf::Time &elapsed)
     {
         if(player_bounds.left + player_bounds.width <= window_bound_right)
         {
-            move(speed * elapsed.asSeconds(), 0);
+            move(speed_ * elapsed.asSeconds(), 0);
         }
 
     }
@@ -33,7 +50,7 @@ void Player::animate(const sf::Time &elapsed)
     {
         if(player_bounds.top >= window_bound_top)
         {
-            move(0, -speed * elapsed.asSeconds());
+            move(0, -speed_ * elapsed.asSeconds());
         }
     }
 
@@ -41,7 +58,22 @@ void Player::animate(const sf::Time &elapsed)
     {
         if(player_bounds.top + player_bounds.height <= window_bound_bottom)
         {
-            move(0, speed * elapsed.asSeconds());
+            move(0, speed_ * elapsed.asSeconds());
         }
     }
+
+//    if(shooting_time_ <= 0)
+//    {
+//        shooting_time_ = 1;
+//        shoot();
+//    }
+//    else
+//    {
+//        shooting_time_ -= elapsed.asSeconds();
+//    }
+}
+
+void Enemy_red::animate(const sf::Time &elapsed)
+{
+    move(0, speed_ * elapsed.asSeconds());
 }

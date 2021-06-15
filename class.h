d1700feb1950fb0b97c7308981019051ca8043c2
div-57 <dawid050702000@gmail.com>
2021-06-15 08:15:55 +0200

    Update class.h

--- class.h
+++ class.h
@@ -7,24 +7,44 @@
 class Game : public sf::Sprite
 {
 public:
+    Game() : Sprite() {};
+    virtual ~Game() = default;
+
     int window_bound_left;
     int window_bound_right;
     int window_bound_top;
     int window_bound_bottom;
     void setWindowBounds(int, int, int, int);
+
+protected:
+
 };
 
 class Player : public Game
 {
 private:
-    int HP = 100;
-    int speed = 400;
+    int HP_;
+    int speed_;
+    int bullet_speed_ = 600;
+    float shooting_time_ = 1.0;
 public:
-    Player(){};
+    Player() {};
+    Player(sf::Texture &texture, const int &resolution_x, const int &resolution_y, int HP, int speed);
     void animate(const sf::Time &elapsed);
 };
 
-class Enemy : public Game
+class Enemy_red : public Game
+{
+private:
+    int HP_;
+    int speed_;
+
+public:
+    Enemy_red(sf::Texture &texture, const int &resolution_x, int HP, int speed);
+    void animate(const sf::Time &elapsed);
+};
+
+class Enemy_gray : public Game
 {
 
 };

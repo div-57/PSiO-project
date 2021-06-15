044c00ead890ea75955fc0c90fe076ef2841aa84
div-57 <dawid050702000@gmail.com>
2021-06-15 08:17:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,36 +5,38 @@
 
 #include "class.h"
 
+sf::Texture load_texture(const std::string &path)
+{
+    sf::Texture texture;
+    if(!texture.loadFromFile(path))
+    {
+        std::cerr<<"Could not load texture"<<std::endl;
+    }
+
+    return texture;
+}
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1366, 768), "Air Battle");
+    int resolution_x = 1366;
+    int resolution_y = 768;
+
+    sf::RenderWindow window(sf::VideoMode(resolution_x, resolution_y), "Air Battle");
     window.setFramerateLimit(60);
 
-    sf::Texture tex_player_plane;
-    sf::Texture tex_sea;
+    sf::Texture tex_player_plane = load_texture("img/player_plane.png");
+    sf::Texture tex_enemy_plane_red = load_texture("img/enemy_plane_red.png");
+    sf::Texture tex_sea = load_texture("img/sea.png");
+    tex_sea.setRepeated(true);
 
     sf::Image icon;
     icon.loadFromFile("img/player_plane.png");
     window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 
-    if(!tex_player_plane.loadFromFile("img/player_plane.png"))
-    {
-        std::cerr<<"Could not load texture"<<std::endl;
-        return 1;
-    }
+    Player player(tex_player_plane, resolution_x, resolution_y, 100, 400);
 
-    if(!tex_sea.loadFromFile("img/sea.png"))
-    {
-        std::cerr<<"Could not load texture"<<std::endl;
-        return 1;
-    }
-    tex_sea.setRepeated(true);
-
-    Player player;
-    player.setTexture(tex_player_plane);
-    player.setPosition((window.getSize().x/2) - 60, 600);
-
-    player.setWindowBounds(0, window.getSize().x, 0, window.getSize().y);
+    Enemy_red enemy(tex_enemy_plane_red, resolution_x, 100, 50);
+    Enemy_red enemy_2(tex_enemy_plane_red, resolution_x, 100, 50);
 
     sf::Sprite sea;
     sea.setTexture(tex_sea);
@@ -56,17 +58,26 @@ int main()
 
         sf::Time elapsed = clock.restart();
         sea.move(0, 35 * elapsed.asSeconds());
-
         if(sea.getPosition().y >= -150)
         {
             sea.setPosition(0, -300);
         }
 
         player.animate(elapsed);
+        enemy.animate(elapsed);
+        enemy_2.animate(elapsed);
+
+        if(player.getGlobalBounds().intersects(enemy.getGlobalBounds()) || player.getGlobalBounds().intersects(enemy_2.getGlobalBounds()))
+        {
+            window.close();
+            std::cout<<"Przegrales"<<std::endl;
+        }
 
         window.clear(sf::Color::Black);
 
         window.draw(sea);
+        window.draw(enemy);
+        window.draw(enemy_2);
         window.draw(player);
 
         window.display();

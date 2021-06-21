23484db4ec507731fab4a99cf03590a1b3e45c65
div-57 <dawid050702000@gmail.com>
2021-06-21 16:18:26 +0200

    Update class.h

--- class.h
+++ class.h
@@ -6,9 +6,19 @@
 
 class Game : public sf::Sprite
 {
+private:
+    int score_ = 0;
+    int wave_size_ = 1;
+    int score_in_size_ = 5;
+    int score_in_wave = 1;
+
+    float shooting_time_ = 0.4;
+    float sum_of_time_ = 0.35;
+
 public:
     Game() : Sprite() {};
     virtual ~Game() = default;
+    virtual void animate(const sf::Time &elapsed) = 0;
 
     int window_bound_left;
     int window_bound_right;
@@ -16,8 +26,21 @@ public:
     int window_bound_bottom;
     void setWindowBounds(int, int, int, int);
 
-protected:
+    int check_score();
+    int wave_size();
+    void add_point();
+    void upgrade_wave_size();
+    void upgrade_next_wave();
+    void upgrade_score_in_wave();
+    int get_score_in_wave();
+    void reset_score_in_wave();
+    void upgrade_score_in_size();
+    int get_score_in_size();
 
+    float get_sum_of_time();
+    float get_shooting_time();
+    void reset_sum_of_time();
+    float update_sum_of_time(const sf::Time &elapsed);
 };
 
 class Player : public Game
@@ -25,11 +48,20 @@ class Player : public Game
 private:
     int HP_;
     int speed_;
-    int bullet_speed_ = 600;
-    float shooting_time_ = 1.0;
+
 public:
     Player() {};
     Player(sf::Texture &texture, const int &resolution_x, const int &resolution_y, int HP, int speed);
+    void animate(const sf::Time &elapsed);
+};
+
+class Bullet : public Game
+{
+private:
+    int bullet_speed_ = 600;
+
+public:
+    Bullet(sf::Texture &texture, sf::FloatRect player_bounds);
     void animate(const sf::Time &elapsed);
 };
 
@@ -42,6 +74,9 @@ private:
 public:
     Enemy_red(sf::Texture &texture, const int &resolution_x, int HP, int speed);
     void animate(const sf::Time &elapsed);
+
+    int get_HP();
+    int get_damage(int damage);
 };
 
 class Enemy_gray : public Game
